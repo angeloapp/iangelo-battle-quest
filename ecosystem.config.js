8ef8b500ac5bf9547e45c1c9e735c88b6db0391b
angeloapp <angeloapp@users.noreply.github.com>
2026-06-08 10:12:06 -0700

    Limitar memoria de Battle Quest para beta segura

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
     instances: 1,
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
+    max_memory_restart: '450M',
     exec_mode: 'fork',
     env: {
       NODE_ENV: 'development',
