1875419b933448f526d55efa6b29bbf4fc936ec4
angeloapp <angeloapp@users.noreply.github.com>
2026-06-09 15:12:51 -0700

    Exponer estado de powerups para HUD movil

--- client/src/app/game/objects/powerups.js
+++ client/src/app/game/objects/powerups.js
@@ -1,6 +1,5 @@
 import Phaser from "phaser";
 
-
 export default class Powerups  {
 	constructor(props){
 		this.items = {
@@ -11,10 +10,27 @@ export default class Powerups  {
 
 		this.scene = props.scene;
 		this.maxItemsPerType = 3;
+		this.syncExternalHUD();
+	}
+
+	syncExternalHUD(){
+		try {
+			if (typeof window !== "undefined") {
+				window.__IBQ_POWERUPS__ = Object.assign({}, this.items);
+				window.dispatchEvent(new CustomEvent("ibq:powerups", { detail: window.__IBQ_POWERUPS__ }));
+			}
+		} catch (error) {
+			console.log("No se pudo sincronizar HUD externo de powerups", error);
+		}
 	}
 
 	useItem(item){
+		if(!this.items.hasOwnProperty(item)) {
+			return false;
+		}
+
 		if(this.items[item] == 0) {
+			this.syncExternalHUD();
 			return false;
 		}
 
@@ -42,24 +58,31 @@ export default class Powerups  {
 
 			default:
 			console.log("objeto desconocido usado: " + item);
+			return false;
 		}
 
-		this.items[item] -= 1;
+		this.items[item] = Math.max(0, this.items[item] - 1);
 
 		this.scene.events.emit("item_changed", this.items);
+		this.syncExternalHUD();
+		return true;
 	}
 
 	collectItem(item){
+		if(!this.items.hasOwnProperty(item)) {
+			return false;
+		}
+
 		if (this.items[item] >= this.maxItemsPerType) {
 			this.scene.events.emit("item_changed", this.items);
+			this.syncExternalHUD();
 			return false;
 		}
 
 		this.items[item] += 1;
 
 		this.scene.events.emit("item_changed", this.items);
+		this.syncExternalHUD();
 		return true;
 	}
-
-
 }
